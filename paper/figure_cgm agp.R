909f1b6ac470e350e817c3937e265fbe81088cc7
Varghese <JVARGH7@emory.edu>
2022-12-20 07:55:37 -0500

    Added combined plot of CGM and glucose-insulin

--- paper/figure_cgm agp.R
+++ paper/figure_cgm agp.R
@@ -5,18 +5,30 @@ files = files[regexpr("\\.csv",files)>0]
 
 library(lubridate)
 source("C:/code/external/functions/cgm/plot_agp.R")
-
+timestamp_ranges = data.frame()
 pdf(paste0(path_sh_folder,"/Glucose and Insulin Data/figures/figure_cgm agp.pdf"),width=12,height=8)
 
 for (f in files){
   
   df = read_csv(paste0(inputdirectory,f)) %>% 
     mutate(sensorglucose = case_when(sensorglucose == "Low" ~ NA_real_,
-                                     TRUE ~ as.numeric(sensorglucose)))
+                                     TRUE ~ as.numeric(sensorglucose)) )
+  
+
+  
+  
   if(!is.POSIXct(df$timestamp) & ! is.Date(df$timestamp)){
     df = df %>% 
       mutate(timestamp = mdy_hms(timestamp))
   }
+  
+  timestamp_ranges = bind_rows(timestamp_ranges,
+                               df %>% 
+                                 summarize(min = min(timestamp,na.rm=TRUE),
+                                           max = max(timestamp,na.rm=TRUE),
+                                           n = sum(!is.na(sensorglucose))
+                                           ) %>% 
+                                 mutate(file = f))
   
   df  %>% 
     plot_agp(.,title=str_replace(f,"\\.csv",""),y_label = "Sensor Glucose (mg/dL)") %>% 
@@ -25,3 +37,5 @@ for (f in files){
 }
 
 dev.off()
+
+write_csv(timestamp_ranges,"data/cgm timestamp ranges.csv")
--- paper/figure_glucose and insulin over time.R
+++ paper/figure_glucose and insulin over time.R
@@ -6,7 +6,10 @@ or_to_icu_ivinsulin <- readRDS(paste0(path_sh_folder,"/Glucose and Insulin Data/
 or_to_icu_bolus <- readRDS(paste0(path_sh_folder,"/Glucose and Insulin Data/working/or_to_icu_bolus.RDS")) %>% 
   mutate(timestamp = paste0(date_surgery," ",time_bolus) %>% ymd_hms(.))  %>% ungroup()
 
-
+timestamp_ranges = read_csv("data/cgm timestamp ranges.csv") %>% 
+  mutate(record_id = str_replace(file,"(_|\\.).*","")) %>% 
+  group_by(record_id) %>% 
+  dplyr::filter(n == min(n))
 
 icu48h_glucose <- readRDS(paste0(path_sh_folder,"/Glucose and Insulin Data/working/icu48h_glucose.RDS"))  %>% 
   mutate(timestamp = paste0(date_calendar," ",time_icu) %>% ymd_hms(.)) %>% ungroup()
@@ -118,6 +121,80 @@ for(u_r in unique_records){
 
 
 dev.off()
+
+
+# Combined CGM and Glucose Insulin Plot --------
+
+unique_records_combined <- timestamp_ranges$record_id
+unique_files_combined <- timestamp_ranges$file
+library(ggpubr)
+source("C:/code/external/functions/cgm/plot_agp.R")
+
+
+pdf(paste0(path_sh_folder,"/Glucose and Insulin Data/figures/figure_cgm and glucose insulin over time.pdf"),width=12,height=8)
+
+for(i in 1:nrow(timestamp_ranges)){
+  
+  u_r_df <- fig_df %>% 
+    dplyr::filter(record_id == unique_records_combined[i]) %>% 
+    mutate(timestamp = as_datetime(timestamp))
+  
+  inputdirectory = paste0(path_sh_folder,"/Glucose and Insulin Data/raw/CGM/")
+  
+  cgm_df = read_csv(paste0(inputdirectory,unique_files_combined[i])) %>% 
+    mutate(sensorglucose = case_when(sensorglucose == "Low" ~ NA_real_,
+                                     TRUE ~ as.numeric(sensorglucose)) )
+  
+  if(!is.POSIXct(cgm_df$timestamp) & ! is.Date(cgm_df$timestamp)){
+    cgm_df = cgm_df %>% 
+      mutate(timestamp = mdy_hms(timestamp))
+  }
+  
+  min_timestamp = min(c(cgm_df$timestamp,u_r_df$timestamp))
+  max_timestamp = max(c(cgm_df$timestamp,u_r_df$timestamp))
+  
+  
+  
+  if(nrow(u_r_df)>0){
+    figA = u_r_df %>% 
+      ggplot(data=.,
+             aes(x=timestamp,y=value,col=variable,shape=type))+ 
+      geom_point() +
+      geom_path(aes(linetype=location)) +
+      theme_bw() +
+      ylab("") +
+      xlab("Timestamp") +
+      ggtitle(paste0("Patient: ",unique_records_combined[i])) +
+      scale_color_manual(name="",values=c("Glucose"="red","Insulin"="darkblue")) +
+      scale_shape_manual(name="",values=c("POCT"=1,"IV"=2,"Bolus"=3,"Subcutaneous"=4,"Meal"=5)) +
+      scale_linetype_manual(name="",values=c("ICU" = 2,"OR"=1)) +
+      scale_y_continuous(limits=c(0,300),breaks=seq(0,300,by=50)) +
+      scale_x_datetime(date_labels = "%d-%b (%H:%M)",limits = c(min_timestamp,max_timestamp)) +
+      geom_hline(yintercept = c(54,250),
+                 color="orange") +
+      geom_hline(yintercept = c(70,180),
+                 color="darkgreen") 
+    
+    # CGM Plot -----------
+    
+    figB = cgm_df  %>% 
+      plot_agp(.,title=str_replace(unique_files_combined[i],"\\.csv",""),y_label = "Sensor Glucose (mg/dL)",
+               x_limits = c(min_timestamp,max_timestamp))
+    
+    ggarrange(figA,figB,
+              nrow=2,ncol=1) %>% 
+      print(.)
+    
+    
+  }
+  
+  
+}
+
+
+dev.off()
+
+
 
 
 
